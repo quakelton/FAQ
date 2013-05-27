da0febe4e59e64f84faa37324f698252510c8d7d
Elton Chung <elton328@gmail.com>
2013-05-27 15:28:10 +0800

    Show the first time install message and warnings.

--- themes/admin/default/home.php
+++ themes/admin/default/home.php
@@ -13,14 +13,14 @@
 </script>//-->
 
 
-	<?php /*if(isset($first_time)) { ?>
-		<div class="warning"><p><?php echo lang('kb_first_time'); ?></p></div>
+    <?php if(isset($first_time)) { ?>
+	<div class="warning"><p><?php echo lang('kb_first_time'); ?></p></div>
     <?php } elseif($install==TRUE) { ?>
     	<div class="warning"><p>Please delete the includes/application/controllers/setup folder.</p></div>
     <?php } ?>
-    <?php if($settings['version'] <> $latest) { ?>
-		<div class="warning"><p><?php echo $latest; ?> <?php echo lang('kb_update_1'); ?> <a href="http://68kb.com/download/"><?php echo lang('kb_update_2'); ?></a>.</p></div>
-	<?php }*/ ?>
+    <?php /*if($settings['version'] <> $latest) { ?>
+	<div class="warning"><p><?php echo $latest; ?> <?php echo lang('kb_update_1'); ?> <a href="http://68kb.com/download/"><?php echo lang('kb_update_2'); ?></a>.</p></div>
+    <?php }*/ ?>
 	
     
 <h2><?php echo lang('kb_welcome'); ?> <?php //echo $username; ?></h2>
